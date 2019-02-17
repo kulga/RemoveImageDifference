d5cf7bb82d2fd67cad92c697acdc1a6dd77b828a
Unknown <sagsdf_github_tyler71@xyzz.work>
2019-02-16 21:30:51 -0800

    Make average mode configurable
    reuse mode average

--- image_difference.py
+++ image_difference.py
@@ -27,7 +27,7 @@ class ImageArray():
         print(f'Saved to {save_location}')
 
     def _process_image(self):
-        pass
+        raise AttributeError('Should be defined in subclass')
 
 
 class ImageMedian(ImageArray):
@@ -51,13 +51,86 @@ class ImageMean(ImageArray):
         flattened_mean_image_array = np.mean(stacked_image_arrays, axis=0)
         return flattened_mean_image_array
 
+class ImageMode(ImageArray):
+    '''Collapse image array into mode of each pixel'''
+    def __init__(self, images):
+        super().__init__(images)
+
+    def _process_image(self, stacked_image_arrays):
+        def mode(ndarray, axis=0):
+            # https://stackoverflow.com/a/35674754/4172336 by devdev
+            # Check inputs
+            ndarray = np.asarray(ndarray)
+            ndim = ndarray.ndim
+            if ndarray.size == 1:
+                return (ndarray[0], 1)
+            elif ndarray.size == 0:
+                raise Exception('Cannot compute mode on empty array')
+            try:
+                axis = range(ndarray.ndim)[axis]
+            except:
+                raise Exception('Axis "{}" incompatible with the {}-dimension array'.format(axis, ndim))
+
+            # If array is 1-D and np version is > 1.9 np.unique will suffice
+            if all([ndim == 1,
+                    int(np.__version__.split('.')[0]) >= 1,
+                    int(np.__version__.split('.')[1]) >= 9]):
+                modals, counts = np.unique(ndarray, return_counts=True)
+                index = np.argmax(counts)
+                return modals[index], counts[index]
+
+            # Sort array
+            sort = np.sort(ndarray, axis=axis)
+            # Create array to transpose along the axis and get padding shape
+            transpose = np.roll(np.arange(ndim)[::-1], axis)
+            shape = list(sort.shape)
+            shape[axis] = 1
+            # Create a boolean array along strides of unique values
+            strides = np.concatenate([np.zeros(shape=shape, dtype='bool'),
+                                         np.diff(sort, axis=axis) == 0,
+                                         np.zeros(shape=shape, dtype='bool')],
+                                        axis=axis).transpose(transpose).ravel()
+            # Count the stride lengths
+            counts = np.cumsum(strides)
+            counts[~strides] = np.concatenate([[0], np.diff(counts[~strides])])
+            counts[strides] = 0
+            # Get shape of padded counts and slice to return to the original shape
+            shape = np.array(sort.shape)
+            shape[axis] += 1
+            shape = shape[transpose]
+            slices = [slice(None)] * ndim
+            slices[axis] = slice(1, None)
+            # Reshape and compute final counts
+            counts = counts.reshape(shape).transpose(transpose)[slices] + 1
+
+            # Find maximum counts and return modals/counts
+            slices = [slice(None, i) for i in sort.shape]
+            del slices[axis]
+            index = np.ogrid[slices]
+            index.insert(axis, np.argmax(counts, axis=axis))
+            return sort[index], counts[index]
+        return mode(stacked_image_arrays)[0]
+
 
 if __name__ == '__main__':
+    average_methods = {
+            'median': ImageMedian,
+            'mean': ImageMean,
+            'mode': ImageMode,
+            }
+
     parser = argparse.ArgumentParser('Merge Common Pictures')
     parser.add_argument('-o', '--output',
-            default='output.jpg')
+            default='output.jpg',
+            )
+    parser.add_argument('-t', '--type',
+            type=str,
+            choices=average_methods.keys(),
+            default='median',
+            help='Default Median',
+            )
     args, source_images = parser.parse_known_args()
 
-    DemoImages = ImageMedian(source_images)
-    #DemoImages = ImageMean(source_images)
+
+    DemoImages = average_methods[args.type](source_images)
     DemoImages.save_image(args.output)
